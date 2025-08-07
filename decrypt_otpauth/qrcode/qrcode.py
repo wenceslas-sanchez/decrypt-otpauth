5f475daedcad69b275b5234b8e89df70e4e1112c
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2025-08-07 17:18:34 -0400

    ✨ add QR code generation from API

---
+++ decrypt_otpauth/qrcode/qrcode.py
@@ -0,0 +1,40 @@
+import urllib.parse
+import urllib.request
+from PIL import Image
+import io
+
+
+def fetch_and_display(uri: str) -> None:
+    qr_code = _fetch_qrcode(uri)
+    _display_qr_terminal(qr_code)
+
+
+def _display_qr_terminal(image_data):
+    buffer = io.BytesIO(image_data)
+    img = Image.open(buffer).convert("L").resize((40, 40))
+
+    for y in range(img.height):
+        for x in range(img.width):
+            pixel = img.getpixel((x, y))
+            print("██" if pixel < 128 else "  ", end="")
+        print()
+
+
+def _fetch_qrcode(uri) -> bytes:
+    qr_url = _create_qr_url_via_api(uri)
+    with urllib.request.urlopen(qr_url) as response:
+        image_data = response.read()
+        # headers = response.info()
+    return image_data
+
+
+def _create_qr_url_via_api(uri: str):
+    base_url = "https://api.qrserver.com/v1/create-qr-code/"
+    params = urllib.parse.urlencode(
+        {
+            "data": uri,
+            "ecc": "L",  # Error correction level
+            "size": "200x200",
+        }
+    )
+    return f"{base_url}?{params}"
---
+++ decrypt_otpauth/qrcode/uri.py
@@ -0,0 +1,48 @@
+import base64
+from typing import Protocol
+from urllib.parse import quote, urlencode
+
+from decrypt_otpauth.otpauth.account import Account
+
+
+class UriGenerator(Protocol):
+    """Protocol for generating OTP URIs."""
+
+    def get_uri(self, *args, **kwargs) -> str:
+        """Generate the complete OTP URI."""
+
+    def get_uri_label(self, *args, **kwargs) -> str:
+        """Generate the label part of the URI."""
+
+    def get_uri_parameters(self, *args, **kwargs) -> str:
+        """Generate the query parameters part of the URI."""
+
+
+class OtpUriGenerator:
+    """Standard OTP URI generator implementation."""
+
+    def __init__(self, scheme: str = "otpauth"):
+        self._scheme = scheme
+
+    def get_uri(self, account: Account) -> str:
+        uri_label = self.get_uri_label(account)
+        uri_params = self.get_uri_parameters(account)
+        return f"{self._scheme}://{account.type.uri_value}/{uri_label}?{uri_params}"
+
+    def get_uri_label(self, account: Account) -> str:
+        return quote(f"{account.issuer}:{account.label}")
+
+    def get_uri_parameters(self, account: Account) -> str:
+        parameters = {
+            "secret": self._decode_secret(account.secret),
+            "algorithm": account.algorithm.uri_string,
+            "period": account.period,
+            "digits": account.digits,
+            "issuer": account.issuer,
+            "counter": account.counter,
+        }
+        return urlencode(parameters)
+
+    @staticmethod
+    def _decode_secret(secret: bytes) -> str:
+        return base64.b32encode(secret).decode("utf-8").rstrip("=")
