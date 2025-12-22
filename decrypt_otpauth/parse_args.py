45554cfa4913e19d1ea26ffda77dcdffce66926a
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2025-12-22 13:01:17 -0500

    ✨ add `--images-location`

--- decrypt_otpauth/parse_args.py
+++ decrypt_otpauth/parse_args.py
@@ -11,4 +11,11 @@ def parse_args() -> argparse.Namespace:
         required=True,
         help="path to your encrypted OTP Auth backup (.otpauthdb)",
     )
+    parser.add_argument(
+        "--images-location",
+        action="store",
+        type=str,
+        required=False,
+        help="directory path to save QR code images instead of displaying them in terminal",
+    )
     return parser.parse_args()
